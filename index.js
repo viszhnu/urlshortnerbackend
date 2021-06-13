388f744f9110db21b7c61d932829304aa4e4e9e2
viszhnu <58871779+viszhnu@users.noreply.github.com>
2021-06-13 16:07:14 +0530

    commit 2

--- index.js
+++ index.js
@@ -8,81 +8,119 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const mongodb=require("mongodb");
+const mongodb = require("mongodb");
 const { nanoid } = require("nanoid");
-const URL="mongodb+srv://vishnu:n3Uf35jUEa!4qEr@cluster0.xfblp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const DBname="urlshortner";
+const URL =
+  "mongodb+srv://vishnu:n3Uf35jUEa!4qEr@cluster0.xfblp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const DBname = "urlshortner";
 
-app.post("/", async function(req, res){
+app.post("/", async function (req, res) {
+  try {
+    let connection = await mongodb.connect(URL, { useUnifiedTopology: true });
 
-    try{
-        let connection = await mongodb.connect(URL);
+    let db = await connection.db(DBname);
+    console.log(req.body);
 
-        let db = await connection.db(DBname);
-        
-        let bigurl = req.body;
-        
-        let randomstring=nanoid(5);
-        
-        req.body.shorturl = randomstring;
-        await db.collection("urls").insertOne(req.body);
+    db.collection("urls").findOne(
+      { longurl: req.body.longurl },
+      async function (err, data) {
+        if (err) {
+          throw err;
+        } else {
+          console.log(typeof data, data);
+          if (data !== null) {
+            res.json({
+              shorturl: data.shorturl,
+            });
+          } else {
+            let bigurl = req.body;
 
-        await connection.close();
+            let randomstring = nanoid(5);
 
-        res.json({
-            shorturl:randomstring
-        })
-    }catch(e){
-        res.json({
-            message:"error in url post"
-        })
+            req.body.shorturl = randomstring;
+
+            await db.collection("urls").insertOne(req.body);
+
+            await connection.close();
+
+            res.json({
+              shorturl: randomstring,
+            });
+          }
+        }
+      }
+    );
+  } catch (e) {
+    console.log(e);
+    res.json({
+      message: "error in url post",
+    });
+  }
+});
+
+app.get("/", async function (req, res) {
+  try {
+    let connection = await mongodb.connect(URL, {useUnifiedTopology:true});
+
+    let db = await connection.db(DBname);
+
+    let answer = await db.collection("urls").find().toArray();
+
+    await connection.close();
+
+    console.log(answer);
+
+    return res.json({
+      answer,
+    });
+  } catch {
+    console.log("error in get");
+  }
+});
+
+app.get("/:id", async function (req, res) {
+  try {
+    let connection = await mongodb.connect(URL, { useUnifiedTopology: true });
+
+    let db = await connection.db(DBname);
+
+    db.collection("urls").findOne(
+      { shorturl: req.params.id },
+      function (err, data) {
+        if (err) {
+          throw err;
+        } else {
+          if (data !== null) res.redirect(data.longurl);
+          else
+            res.json({
+              message: "page not found",
+            });
+        }
+      }
+    );
+  } catch (e) {
+    res.json({
+      message: "error in get",
+    });
+  }
+});
+
+app.delete("/:id", async function (req, res) {
+    try {
+      let connection = await mongodb.connect(URL, {useUnifiedTopology:true});
+  
+      let db = await connection.db(DBname);
+  
+      let answer = await db.collection("urls").deleteOne({_id:mongodb.ObjectID(req.params.id)});
+  
+      await connection.close();
+  
+      //console.log(answer);
+  
+      return res.json({
+        message:"deleted"
+      });
+    } catch {
+      console.log("error in get");
     }
-})
-
-app.get("/", async function(req,res){
-    
-    try{
-        let connection = await mongodb.connect(URL);
-
-        let db = await connection.db(DBname);
-        
-        let answer = await db.collection("urls").find().toArray();
-
-        await connection.close();
-
-        console.log(answer);
-
-        return res.json({
-            answer
-        })
-        
-    }catch{
-        console.log("error in get");
-    }
-})
-
-app.get("/:id", async function (req, res){
-
-    try{
-        let connection = await mongodb.connect(URL, {useUnifiedTopology: true});
-
-        let db = await connection.db(DBname);
-
-        db.collection("urls").findOne({shorturl:req.params.id}, function(err, data){
-            if(err){
-                throw err;
-            }else{
-                if(data!==null)
-                res.redirect(data.longurl);
-                else
-                res.json({
-                    message:"page not found"
-                })
-            }
-        })
-    }catch(e){
-        res.json({
-            message:"error in get"
-        })
-    }
-})
+  });
